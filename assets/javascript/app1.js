aec29bdf6770bccb1b923bba269b1d8bb9a54db0
Stepan <stepos36@gmail.com>
2018-12-25 12:09:25 -0500

    Amazon fresh cart functioning

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -15,16 +15,48 @@ function displayRecipe(){
             nameDiv.append(result[0].strMeal)
             descriptionDiv.append(result[0].strInstructions)
             
-            var ingredients = [];
+            var ingredientsDisplay = [];
+            var ingredients = []
+            var quantities = []
             for (var j = 1; j <= 20; j++) {
                 var ingredient = result[0][`strIngredient${j}`];
+                var quantity = result[0][`strMeasure${j}`]
+                if (quantity) {
+                    quantities.push(quantity)
+                }
                 if (ingredient) {
-                    ingredients.push(ingredient);
+                    ingredientsDisplay.push(' '+ingredient+'('+quantity+')');
+                    ingredients.push(ingredient)
                 }
             }
-            $('#ingredients').html('<strong>Ingredients: </strong>'+ingredients.toString())
+            $('#ingredients').html('<strong>Ingredients: </strong>'+ingredientsDisplay)
+            
+            var amazonJson = {
+                "ingredients": []
+            }
+            for(q=0;q<ingredients.length;q++) {
+                var ingredientJSON = {
+                    "name": "",
+                    "quantityList": [
+                      {
+                        "unit": "COUNT",
+                        "amount": 1
+                      },
+                      {
+                        "unit": "KILOGRAMS",
+                        "amount": 0.1
+                      }
+                    ]
+                  }
+                var productName = ingredients[q]
+                var productQuantity = quantities[q]
+                ingredientJSON[`name`] = productName
+                amazonJson.ingredients.push(ingredientJSON)
+            }
+            $('#amazon-input').val(JSON.stringify(amazonJson))
     })
 }
+    
 $(document).ready(function() {
     displayRecipe()
-})
+    })
