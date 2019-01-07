4bdac225bd4c93c2085af4b403aabc2e8c4a4903
Stepan <stepos36@gmail.com>
2019-01-07 18:18:25 -0500

    massive css changes//indexedDB in use//javascript cleaned up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,50 +1,121 @@
+/* Notes:
+`blah blah ${javascript expression}` -> template literals (ES6+ feature)
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+instead of 'blah blah ' + (javascript expression/value) -> string concatenation using '+'
+*/
 
-function displayRecipe(){
-    var request = $('#search-input').val()
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+request;
-    console.log(queryURL)
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        var result = response.meals;
-        for (var i=0;i<result.length;i++) {
-            var recipeLink = $('<a>')
-            var recipeLine = $('<div class="card" style="height:32rem;width:20rem;margin:20px 5px 24px 5px;display:inline-block;vertical-align: top">');
-            var cardBodyDiv = $('<div class="card-body">')
-            var imageDiv = $('<img class="card-img-top meal-image" alt="image" style="width:100%">')
-            var nameDiv = $('<div class="name">')
-            var descriptionDiv = $('<div class=description>')
-            var name = result[i].strMeal
-            var class1 = name.replace(/\s/g, '')
-            imageDiv.attr('src', result[i].strMealThumb)
-            nameDiv.append('<h3>'+result[i].strMeal+'</h3>')
-            $(document).on( "click", '#' +class1, function() {
-                sessionStorage.setItem("dishName", $(this).attr('fullName'))
-            })
-            descriptionDiv.append(result[i].strInstructions)
-            descriptionDiv.text(descriptionDiv.text().substring(0,150)).append('...')
-            cardBodyDiv.append(nameDiv).append(descriptionDiv)
-            recipeLine.append(imageDiv).append(cardBodyDiv)
-            recipeLink.append(recipeLine).attr('href', 'recipe.html').attr('target', '_blank').attr('id', class1).attr('fullName', name).css('cursor:pointer')
-            $('#result-table').append(recipeLink).attr('style', 'text-align:center; justify-content: center')
-            var ingredients = [];
-            for (var j = 1; j <= 20; j++) {
-                var ingredient = result[i][`strIngredient${j}`];
-                if (ingredient) {
-                    ingredients.push(ingredient);
-                }
-            }
-            localStorage.setItem("request",request)
-        }
-    })
+function createMealCard(mealId, imgSrc, name, desc) {
+  var cardDiv = $('<div>', { 
+    class: 'card meal-card',
+    'data-mealId': mealId 
+  });
+  var imageDiv = $('<img>', { 
+    class: 'meal-image w-100',
+    alt: `${name} image`,
+    src: imgSrc 
+  });
+  var cardBodyDiv = $('<div>', { class: 'card-body' });
+  var nameDiv = $('<div>', { 
+    class: 'meal-name text-center',
+    html: `<h3 class="text-truncate">${name}</h3>` 
+  });
+  var descDiv = $('<div>', { 
+    class: 'meal-desc',
+    text: `${desc.substring(0,150)}...` 
+  });
+  
+  cardBodyDiv.append(nameDiv, descDiv);
+  cardDiv.append(imageDiv, cardBodyDiv);
+
+  return cardDiv;
+}
+
+function displayRecipe() {
+  var request = $('#search-input').val();
+  var queryURL =
+    'https://www.themealdb.com/api/json/v1/1/search.php?s=' + request;
+
+  console.log('queryURL: ', queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    var result = response.meals;
+    for (var i = 0; i < result.length; i++) {
+      var mealId = result[i].idMeal;
+      var imgSrc = result[i].strMealThumb;
+      var name = result[i].strMeal;
+      var desc = result[i].strInstructions;
+      var recipeLink = $('<a>', {
+        href: 'recipe/index.html'        
+      });
+      var mealCard = createMealCard(mealId, imgSrc, name, desc);
+      $('#result-table').append(recipeLink.append(mealCard)).attr('style', 'text-align:center; justify-content: center');
+      localStorage.setItem('request', request);
+    }
+  });
 }
 
 $(document).ready(function() {
-    $(document).on('click', '#search-btn', function(event) {
-        event.preventDefault()
-        $('#result-table').empty()
-        displayRecipe()
-    })
+  $(document).on('click', '#search-btn', function(event) {
+    event.preventDefault();
+    $('#result-table').empty();
+    displayRecipe();
+  });
 
-})
+  $('#result-table').on('mouseover', '.meal-card', function() {
+    let key = $(this).attr('data-mealId');
+    let request = indexedDB.open('dishDatabase', 1),
+    db,
+    tx,
+    store;
+    request.onupgradeneeded = function(e) {
+      let db = request.result,
+      store = db.createObjectStore('dishName', {keyPath: 'dishName1'});
+    };
+    request.onerror = function(e) {
+      console.log('error'+e.targer.errorCode);
+    };
+    request.onsuccess = function(e) {
+      db = request.result;
+      tx = db.transaction("dishName", 'readwrite');
+      store = tx.objectStore('dishName');
+      db.onerror = function(e) { 
+        console.log('error' + e.target.errorCode)
+      }
+      store.put({dishName1: 1, dishName: key})
+      let q1 = store.get(1)
+      q1.onsuccess = function() {
+        console.log(q1.result.dishName)
+      }
+    }
+  });
+  $('#result-table').on('touchstart', '.meal-card', function() {
+    let key = $(this).attr('data-mealId');
+    let request = indexedDB.open('dishDatabase', 1),
+    db,
+    tx,
+    store;
+    request.onupgradeneeded = function(e) {
+      let db = request.result,
+      store = db.createObjectStore('dishName', {keyPath: 'dishName1'});
+    };
+    request.onerror = function(e) {
+      console.log('error'+e.targer.errorCode);
+    };
+    request.onsuccess = function(e) {
+      db = request.result;
+      tx = db.transaction("dishName", 'readwrite');
+      store = tx.objectStore('dishName');
+      db.onerror = function(e) { 
+        console.log('error' + e.target.errorCode)
+      }
+      store.put({dishName1: 1, dishName: key})
+      let q1 = store.get(1)
+      q1.onsuccess = function() {
+        console.log(q1.result.dishName)
+      }
+    }
+  });
+});
--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -1,61 +1,84 @@
-
-function displayRecipe(){
-    var request = sessionStorage.getItem("dishName")
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s="+ request;
-    console.log(queryURL)
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        var result = response.meals;
-            var imageDiv = $('#food-pic')
-            var nameDiv = $('#recipe-name')
-            var descriptionDiv = $('#recipe')
-            imageDiv.attr('src', result[0].strMealThumb)
-            nameDiv.append(result[0].strMeal)
-            descriptionDiv.append(result[0].strInstructions)
-            
-            var ingredientsDisplay = [];
-            var ingredients = []
-            var quantities = []
-            for (var j = 1; j <= 20; j++) {
-                var ingredient = result[0][`strIngredient${j}`];
-                var quantity = result[0][`strMeasure${j}`]
-                if (quantity) {
-                    quantities.push(quantity)
-                }
-                if (ingredient) {
-                    ingredientsDisplay.push(' '+ingredient+'('+quantity+')');
-                    ingredients.push(ingredient)
-                }
-            }
-            $('#ingredients').html('<strong>Ingredients: </strong>'+ingredientsDisplay)
-            var amazonJson = {
-                "ingredients": []
-            }
-            for(q=0;q<ingredients.length;q++) {
-                var ingredientJSON = {
-                    "name": "",
-                    "quantityList": [
-                      {
-                        "unit": "COUNT",
-                        "amount": 1
-                      },
-                      {
-                        "unit": "KILOGRAMS",
-                        "amount": 0.001
-                      }
-                    ]
-                  }
-                var productName = ingredients[q]
-                var productQuantity = quantities[q]
-                ingredientJSON[`name`] = productName
-                amazonJson.ingredients.push(ingredientJSON)
-            }
-            $('#amazon-input').val(JSON.stringify(amazonJson))        
-    })
-}
+function displayRecipe() {
+  let request = indexedDB.open('dishDatabase', 1);
+  request.onsuccess = function(e) {
+    db = request.result;
+    tx = db.transaction("dishName", 'readwrite');
+    store = tx.objectStore('dishName');
+    db.onerror = function(e) { 
+      console.log('error' + e.target.errorCode)
+    }
+    let q1 = store.get(1)
+    q1.onsuccess = function() {
+      console.log(q1.result.dishName)
     
+  
+      //var request = localStorage.getItem('mealId');
+      var queryURL =
+        'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + q1.result.dishName;
+
+      console.log(queryURL);
+
+      $.ajax({
+        url: queryURL,
+        method: 'GET'
+      }).then(function(response) {
+        var result = response.meals;
+        var imageDiv = $('#food-pic');
+        var nameDiv = $('#recipe-name');
+        var descriptionDiv = $('#recipe');
+        imageDiv.attr('src', result[0].strMealThumb);
+        nameDiv.append(result[0].strMeal);
+        descriptionDiv.append(result[0].strInstructions);
+        var ingredientsDisplay = [];
+        var ingredients = [];
+        var quantities = [];
+        console.log(result[0].strYoutube)
+        var video = result[0].strYoutube
+        video1 = video.replace(/watch\?v=/g,'embed/')
+        console.log(video1)
+        $('.mealVideo').append('<iframe class="video" src="'+video1+'" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>')
+        for (var j = 1; j <= 20; j++) {
+          var ingredient = result[0][`strIngredient${j}`];
+          var quantity = result[0][`strMeasure${j}`];
+          if (quantity) {
+            quantities.push(quantity);
+          }
+          if (ingredient) {
+            ingredientsDisplay.push(' ' + ingredient + '(' + quantity + ')');
+            ingredients.push(ingredient);
+          }
+        }
+        $('.ingredients').html(
+          '<strong>Ingredients: </strong>' + ingredientsDisplay
+        );
+        var amazonJson = {
+          ingredients: []
+        };
+        for (q = 0; q < ingredients.length; q++) {
+          var ingredientJSON = {
+            name: '',
+            quantityList: [
+              {
+                unit: 'COUNT',
+                amount: 1
+              },
+              {
+                unit: 'KILOGRAMS',
+                amount: 0.1
+              }
+            ]
+          };
+          var productName = ingredients[q];
+          var productQuantity = quantities[q];
+          ingredientJSON[`name`] = productName;
+          amazonJson.ingredients.push(ingredientJSON);
+        }
+        $('#amazon-input').val(JSON.stringify(amazonJson));
+      });
+    }
+  }
+}
+
 $(document).ready(function() {
-    displayRecipe()
-    })
+  displayRecipe();
+});
