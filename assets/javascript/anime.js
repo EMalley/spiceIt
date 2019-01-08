84d829a35833a0a756b31fa054ec3f249cd1d97a
Stepan <stepos36@gmail.com>
2019-01-07 19:13:55 -0500

    css changes// animate.js added

---
+++ assets/javascript/anime.js
@@ -0,0 +1,89 @@
+console.log("working anime")
+
+$(document).ready(function(){
+// =====================
+// Index Page
+// =====================
+
+
+// =======================
+// AppNameFadesIn 1X1
+// =======================
+var logoTimeline = anime.timeline;
+    logoTimeline({
+        loop:false,
+        // duration:3500,
+        easing: 'easeInOutSine',
+    })
+    .add({
+        targets: '#spice , #dinner',
+        opacity: [0 , 1],
+        duration:800
+    })
+    .add({
+        targets:'#it',
+        // translateY:[
+        //     {vaule:80, duration:800},
+        //     {vaule:0, duration:800}
+        // ],
+        opacity: [0 , 1],
+        duration:800
+    })
+    .add({
+        targets:'#up',
+        opacity: [0 , 1],
+        duration:500,
+        offset:'-=200'
+    })
+    .add({
+        targets: '.topbar-sign',
+        translateX: {vaulue:100 , value: 0},
+        opacity: [.0, 1],
+        // offset: '-=1000'
+    })
+    .add({    
+        targets:'#search-input , #search-btn',
+        easing: 'linear',
+        // offset:'-= 4000',
+        opacity:[0 , 1],
+})
+
+// ============================
+// after Search Btn Is clicked
+// ============================
+var searchBtn = document.querySelector("#search-btn");
+searchBtn.addEventListener('click', function(){
+    console.log("anime clicked, add animations here")
+    var results = anime.timeline;
+    results({
+        targets:'.meal-img',
+        scale:[0 , 1],
+        loop:false,
+        easing: 'linear',
+        duration:5000,
+    })
+})
+
+
+// ===========================
+// Recipe Page
+// ===========================
+var cardTimeline = anime.timeline;
+cardTimeline({
+    loop: false,
+    easing: 'linear'
+})
+.add({
+    targets: '#recipe-name',
+    // scale: [.5 , 1],
+    opacity: [0 , 1],
+    duration: 500,
+})
+.add({
+    targets: '#recipe , #ingredients',
+    scale:[.5, 1],
+    backgroundColor: '#444',
+})
+
+
+})
